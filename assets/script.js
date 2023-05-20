fafa48fed85976ee0fbafe985f9be78f34f61a5f
info-hazard <107716504+info-hazard@users.noreply.github.com>
2023-05-19 18:30:10 -0700

    added js variables for qurstions and answers

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+var questions = [
+    {
+      title: 'Inside which HTML element do we put the JavaScript?',
+      choices: ['<javascript>', '<scripting>', '<js>', '<script>'],
+      answer: '<script>',
+    },
+    {
+      title: 'Where is the correct place to insert a JavaScript?',
+      choices: ['The <head section', 'Both the <head> and <body> section are correct', 'The <body> section', 'The <footer> section'],
+      answer: 'The <body> section',
+    },
+    {
+      title: 'How do you call a function named "myFunction"?',
+      choices: [
+        'myFunction()',
+        'call myFunction()',
+        'function myFunction()',
+        'call function myFunction()',
+      ],
+      answer: 'myFunction()',
+    },
+    {
+      title:
+        'How can you add a comment in Javascript?',
+      choices: ['//This is a comment', '"This is a comment"', '<!--This is a comment--!>', '(This is a comment)'],
+      answer: '//This is a comment',
+    },
+    {
+      title:
+        'Which of the following is correct about features of JavaScript?',
+      choices: ['JavaScript is a lightweight, interpreted programming language.', 'JavaScript is designed for creating network-centric applications.', 'JavaScript is complementary to and integrated with Java.', 'All of the above.'],
+      answer: 'All of the above.',
+    },
+  ];
+  
