d0ff81fb5a565c822a3ef99397022d4b4035980c
info-hazard <107716504+info-hazard@users.noreply.github.com>
2023-05-20 01:41:36 -0700

    added Bootstrap to both html pages and added some java script variables to reference dom elements in html

--- assets/script.js
+++ assets/script.js
@@ -34,5 +34,11 @@ var questions = [
     },
   ];
 
-  
+  //referencing DOM elements
+  var questionsEl = document.getElementById('questions');
+  var timerEl = document.getElementById('time');
+  var choicesEl = document.getElementById('choices');
+  var submitBtn = document.getElementById('submit');
+  var startBtn = document.getElementById('start');
+  var initialsEl = document.getElementById('initials');
   
